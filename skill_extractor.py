6e61fe7f19f987df9ce4749786dbba225f4f95b2
sheyannamontgomery92-prog <sheyannamontgomery92@gmail.com>
2026-03-04 05:22:24 -0800

    Create skill_extractor.py

---
+++ skill_extractor.py
@@ -0,0 +1,13 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def extract_keywords(text):
+    doc = nlp(text)
+    keywords = []
+
+    for token in doc:
+        if token.pos_ in ["NOUN", "PROPN"]:
+            keywords.append(token.text.lower())
+
+    return list(set(keywords))
